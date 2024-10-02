d57088af71f921b5a4270bddfd1bd057bebe9016
Arthur C <78846238+treize-khushrenada@users.noreply.github.com>
2024-10-02 19:46:56 -0400

    update

---
+++ st.py
@@ -0,0 +1,9 @@
+import streamlit as st
+from openai import OpenAI
+
+# Show title and description.
+st.title("📄 Medical Report - AI Translation")
+st.write(
+    "Upload a document below and see it translated in bahasa malaysia "
+)
+
